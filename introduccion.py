4e589b37a992fd0480969d73e34d356b490b0eb0
Oscar Miranda <mgoscar@gmail.com>
2023-06-26 18:04:13 -0600

    Introducción a Python

---
+++ introduccion.py
@@ -0,0 +1,17 @@
+# Este es un comentario de una línea
+
+'''
+Este es un 
+comentario escrito
+en multiples líneas
+'''
+#Imprime o despliega el texto en pantalla
+print("¡Hola Mundo en Python!")
+print("Empezamos desplegando texto en pantalla: ") 
+# También se puede escribiendo entre comillas simples
+print('Así podemos desplegar "comillas dobles"') 
+print("Y de esta manera desplegaremos 'comillas simples'.")
+# Diagonal invertida como carácter de escape
+print("Otra manera es utilizando el carácter de escape, la \"Diagonal Invertida\" -> \' \\ \' ")
+
+#Con "input" podemos pedir al usuario introduzca datos
