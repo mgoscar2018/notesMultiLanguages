047be16a20d88701455f6ba76209443660ce6045
Oscar Miranda <mgoscar@gmail.com>
2023-06-30 20:13:18 -0600

    reorganizando la idea

--- 01_introduccion.py
+++ 01_introduccion.py
@@ -1,3 +1,4 @@
+## COMENTARIOS
 # Este es un comentario de una línea
 
 '''
@@ -5,21 +6,8 @@ Este es un
 comentario escrito
 en multiples líneas
 '''
-#Imprime o despliega el texto en pantalla
-print("¡Hola Mundo en Python!")
-print("Empezamos desplegando texto en pantalla: ") 
-#También se puede escribiendo entre comillas simples
-print('Así podemos desplegar "comillas dobles"') 
-print("Y de esta manera desplegaremos 'comillas simples'.")
-#Diagonal invertida como carácter de escape
-print("Otra manera es utilizando el carácter de escape, la \"Diagonal Invertida\" -> \' \\ \' ")
 
-# En Python podemos redefinir qué queremos utilizar de separador "sep" y qué usar para finalizar "end", quitando el default salto de línea "\n"
-print("Modificándo el comportamiento de print",sep="-",end="*") 
-print("Esto estaría en otra línea, si no se hubiera reemplazado el salto de línea por el asterísco")
-
-
-#Variables
+## VARIABLES
 '''
 Reglas para nombrar variables en Python:
 1. Empieza con una letra o guion bajo “_”
@@ -46,7 +34,26 @@ myvar123 = "Oscar"
 s1, s2, s3 = "texto1", "texto2", "texto3" #Se pueden inicializar multiples variables...
 e1 = e2 = e3 = "Oscar" # y también asignar el mismo valor al mismo tiempo a multiples variables
 
-#Tipos de datos
+# IMPRIMIR O DESPLEGAR DATOS EN PANTALLA
+print("¡Hola Mundo en Python!")
+print("Empezamos desplegando texto en pantalla: ") 
+#También se puede escribiendo entre comillas simples
+print('Así podemos desplegar "comillas dobles"') 
+print("Y de esta manera desplegaremos 'comillas simples'.")
+#Diagonal invertida como carácter de escape
+print("Otra manera es utilizando el carácter de escape, la \"Diagonal Invertida\" -> \' \\ \' ")
+
+# En Python podemos redefinir qué queremos utilizar de separador "sep" y qué usar para finalizar "end", quitando el default salto de línea "\n"
+print("Modificándo el comportamiento de print",sep="-",end="*") 
+print("Esto estaría en otra línea, si no se hubiera reemplazado el salto de línea por el asterísco")
+
+#Imprimir variables
+nombre = "OSCAR"
+print("¡Bienvenido!,", nombre) # Al imprimir una variable utilizando "coma", en automático deja un espacio al concatenar
+print("Te llamas:"+nombre) # pero si se utiliza el símbolo "+", lo deja pegado
+print(f"Mira {nombre}, también puedo meter variales en cadenas pre-formateadas")
+
+## TIPOS DE DATOS
 cadena = 'Esto es una cadena'
 cad2 = "y esta también."
 entero = 2345 # Cualquier número positivo o negativo sin importar el tamaño
@@ -56,7 +63,6 @@ boleanoF = False # Boleano Falso
 nulo = None # Sin valor
 
 # Con "input" podemos pedir al usuario que introduzca datos
-nombre = input("¿Cómo te llamas? ") # ... y lo almacenamos en una variable
-# la cual se podrá utilizar para desplegarla en pantalla de diferentes maneras:
-print("¡Bienvenido!,", nombre) # Al imprimir una variable utilizando "coma", en automático deja un espacio al concatenar
-print("Te llamas:"+nombre) # pero si se utiliza el símbolo "+", lo deja pegado
+edad = input("¿Qué edad tienes? ") # ... y lo almacenamos en una variable
+
+
